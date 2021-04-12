66d9ce7616b5443705453fc998681b05e58a5fab
Varshitha-an <anvarshitha05@gmail.com>
2021-04-12 12:46:54 +0530

    Auth service updated

--- AuthService/Repositories/UserRepository.cs
+++ AuthService/Repositories/UserRepository.cs
@@ -11,8 +11,8 @@ namespace AuthService.Repositories
         
         private static readonly List<UserDto> Users = new List<UserDto>()
         {
-            new UserDto(){UserID = 1,UserName = "John",Password = "john@123"},
-            new UserDto(){UserID = 2,UserName = "Bucky",Password = "bucky@123"}
+            new UserDto(){UserID = 1,UserName = "Ram",Password = "ram@123"},
+            new UserDto(){UserID = 2,UserName = "Sam",Password = "sam@123"}
         };
         public UserDto GetUser(User user)
         {
